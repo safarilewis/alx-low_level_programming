8ca6c1ac567914b235b6d73afa3467935e307b58
safarilewis <lleewwii1@gmail.com>
2022-06-02 11:35:09 -0700

    Trying a different approach to fix the bytes issue

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-char text="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 fwrite(text\n, 59, 1, stderr);
 return (1); }
