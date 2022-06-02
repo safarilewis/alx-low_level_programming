ec428368f0e01021e2821132fe6ab9cedf25092d
safarilewis <lleewwii1@gmail.com>
2022-06-02 10:41:22 -0700

    Printing to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,5 +6,5 @@
  */
 int main(void)
 {
-fwrite(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
 return (1); }  
