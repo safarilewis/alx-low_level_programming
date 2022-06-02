bd62d4ea95510637c68270c68ca734ea802b2a57
safarilewis <lleewwii1@gmail.com>
2022-06-02 05:24:00 -0700

    A file that uses printf to print a sentence

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  *Description: Uses printf() to print a sentence)?
  *Return: Zero
  */
-int main (void)
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf ("with proper grammar, but the outcome is a piece of art, ");
 return(0); }
