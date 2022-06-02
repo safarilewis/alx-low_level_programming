ca893d3586ad6cd21125233e3e195bd0e5f6b3a5
safarilewis <lleewwii1@gmail.com>
2022-06-02 03:49:27 -0700

    This is a C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(void) -Is the main function
+int main(void) /*Description: main() -Is the main function*/ 
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0); }
