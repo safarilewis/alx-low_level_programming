f514e955124592e78f76bd335145acfbd04e5893
safarilewis <lleewwii1@gmail.com>
2022-06-02 11:55:54 -0700

    Trying to find a way to shorten the line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-  char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  fwrite(text, 59, 1, stderr);
+char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ 
+fwrite(txt, 59, 1, stderr);
 return (1); }
