cd1bed0a4e2b479eecf4b55229cf0ea7cc7ad55e
safarilewis <lleewwii1@gmail.com>
2022-06-02 10:49:12 -0700

    Printing to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,9 @@
  *main - Prints to standard error
  (*
  *Description: Thi prints a statement to standard error)?
+ *Return: One
  */
-int main()
+int main(void)
 {
 fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
 return (1); }
