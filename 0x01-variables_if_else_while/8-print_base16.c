c0b047d27a8edc4d38d0bb5261338d46fcaa7fc9
safarilewis <lleewwii1@gmail.com>
2022-06-03 05:43:13 -0700

    Print base16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+(*
+*Description: Printing hexadecimal numbers)?
+*Return: Zero
+*/
+  int main()
+{
+  char ch;
+  char sh;
+  for(ch=0;ch<10;ch++){
+    putchar(ch); }
+  for(sh='a';sh<'g';sh++){
+    putchar(sh);}
+  putchar('\n');
+  return (0);
+}
