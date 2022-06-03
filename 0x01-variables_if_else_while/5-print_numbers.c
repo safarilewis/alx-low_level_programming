1daf8b6d2645a94fdc7126fd1e948b57a56e51b4
safarilewis <lleewwii1@gmail.com>
2022-06-03 04:49:43 -0700

    Print all numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+(*
+*Description: main part of the code)?
+*Return: Zero
+*/
+  int main()
+{
+int x;
+for(x=0;x<10;x++){
+  printf("%d", x);}
+printf("\n");
+return (0); }
