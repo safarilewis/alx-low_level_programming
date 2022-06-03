dad17ad4ec10c11bb64aaf2aacd732b072dfd648
safarilewis <lleewwii1@gmail.com>
2022-06-03 13:25:40 -0700

    Fixing and cleaning code

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,18 +5,18 @@
 *Description: Main function)?
 *Return: Always zero
 */
-  int main()
+int main(void)
 {
-  int n;
-  for(n=0;n<=9;n++)
-    {
-      putchar((n%10) + '0');
-      if(n==9)
-	continue;
-      putchar(',');
-      putchar(' ');
-    }
-  putchar('\n');
-  return (0);
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar((n % 10) + '0');
+if(n == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
     
