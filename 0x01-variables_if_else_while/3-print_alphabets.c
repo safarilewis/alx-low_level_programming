8d77c8bdad2db6e58806d4a57d35330cbe458ee9
safarilewis <lleewwii1@gmail.com>
2022-06-03 03:12:35 -0700

    Prints the alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+(*
+*Description: Looping structure)?
+*Return: returns zero
+*/
+int main()
+{
+  char ch;
+  char sh;
+  for(ch='a';ch<='z';ch++){
+    putchar("%c", ch);}
+  for(sh='A';sh<='Z';sh++){
+    putchar("%c", sh\n);}
+  return (0); }
