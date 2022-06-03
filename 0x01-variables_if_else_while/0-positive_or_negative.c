1c27a9d54d6d69916e03c1715201296e9a9ede21
safarilewis <lleewwii1@gmail.com>
2022-06-03 01:53:12 -0700

    Checks if n is zero, negative, or positive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main- Entry point
+(*
+*Description: Uses if statements to check whether an integer is positive or negative)?
+*Return: Returns zero
+*/
+int main()
+{
+int n;
+ if(n==0){
+   printf("%d is zero", n); }
+ else if(n<0){
+   printf("%d is negative", n); }
+ else{
+   printf("%d is positive", n); }
+return (0); }
