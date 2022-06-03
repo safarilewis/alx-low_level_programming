bdc32b9ef5ac493f7810daf41fa4ced1652749b5
safarilewis <lleewwii1@gmail.com>
2022-06-03 12:51:04 -0700

    Fixing and cleaning code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,5 +21,6 @@ else if (lastdigit == 0)
 printf("Last digit of %d is %d and is 0\n", n, lastdigit); }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit); }
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+}
 return (0); }
