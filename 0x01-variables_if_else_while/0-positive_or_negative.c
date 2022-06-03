66c3aea7c4d047a58e75f940ce120a81f130b7d7
safarilewis <lleewwii1@gmail.com>
2022-06-03 02:12:38 -0700

    Fixing styles

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,26 +2,23 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
 /**
- *maint - Conditionals
+ *main - Conditionals
 (*
 *Description: Checks whether a number is positive or negative)?
 *Return: Returns zero
 */
 int main(void)
 {
-  int n;
-
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  /* your code goes there */
-  if(n<0){
-    printf("%d is negative", n);}
-  else if(n==0){
-    printf("%d is zero", n);}
-  else{
-    printf("%d is positive", n);}
-  return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if(n<0){
+    printf("%d is negative\n", n);}
+else if(n==0){
+    printf("%d is zero\n", n);}
+else{
+    printf("%d is positive\n", n);}
+return (0);
 }
 
