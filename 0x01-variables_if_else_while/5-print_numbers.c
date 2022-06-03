9415d1fffa1038a76d5e088410e6012449757f2b
safarilewis <lleewwii1@gmail.com>
2022-06-03 13:10:26 -0700

    Fixing and cleaning code

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,10 +5,11 @@
 *Description: main part of the code)?
 *Return: Zero
 */
-  int main()
+int main()
 {
 int x;
-for(x=0;x<10;x++){
-  printf("%d", x);}
+for (x=0;x<10;x++)
+{
+printf("%d", x); }
 printf("\n");
 return (0); }
