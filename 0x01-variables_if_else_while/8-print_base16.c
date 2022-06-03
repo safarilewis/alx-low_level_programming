c19d173c6408b33226c867fd0f36f42cfe889236
safarilewis <lleewwii1@gmail.com>
2022-06-03 13:20:58 -0700

    Fixing and cleaning code

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,14 +5,15 @@
 *Description: Printing hexadecimal numbers)?
 *Return: Zero
 */
-  int main()
+int main(void)
 {
-  char ch;
-  char sh;
-  for(ch='0';ch<='9';ch++){
-    putchar(ch); }
-  for(sh='a';sh<'g';sh++){
-    putchar(sh);}
-  putchar('\n');
-  return (0);
-}
+char ch;
+char sh;
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch); }
+for (sh = 'a'; sh < 'g'; sh++)
+{
+putchar(sh);}
+putchar('\n');
+return (0); }
