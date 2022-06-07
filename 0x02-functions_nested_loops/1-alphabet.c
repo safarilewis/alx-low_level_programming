1607d2dde04858a4df3d1816ab213de43bd02a18
safarilewis <lleewwii1@gmail.com>
2022-06-07 02:36:00 -0700

    Fixin alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
 include "main.h"
 /**
- *main - Prints the alphabet in lowercase using putchar only once
+ *print_alphabet - Prints the alphabet in lowercase using putchar only once
  *
  *
  *Return: Always zero
  */
 void print_alphabet(void)
 {
-char c;
-for (c = 'a';c <= 'z';c++){
-putchar(c);
-}
-print_alphabet();
-putchar('\n');
+  char c;
+  for (c = 'a';c <= 'z'; c++)
+    {
+      putchar(c);
+    }
+  putchar('\n');
 }
