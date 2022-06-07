e75ed3fa41cd150cc801f2150a0553502ba047a6
safarilewis <lleewwii1@gmail.com>
2022-06-07 05:39:02 -0700

    Printing numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,19 +5,13 @@
  */
 void print_to_98(int n)
 {
-  if (n >= 98)
+  while (n != 98)
     {
-      for (n = n;n >= 98;n--)
-	{
-	  printf("%d, ", n);
-	}
+      printf("%i, ", n);
+      if (n > 98)
+	n--;
+      else
+	n++;
     }
-      else if (n <= 98) 
-	{
-	  for (n = n;n <= 98;n++)
-	    {
-	      printf("%d, ", n);
-	    }
-	  _putchar ('\n');
-	}
+  printf("98\n");
 }
