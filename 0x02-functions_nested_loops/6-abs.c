8db7ccddcbda57be6864e43353e95ad5c22bb0f1
Safari <lleewwii1@gmail.com>
2022-06-07 22:52:55 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,13 +6,13 @@
  */
 int _abs (int n)
 {
-  if (n >= 0)
-    {
-      return (n);
-    }
-  else
-    {
-      n *= -1;
-      return (n);
-    }
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+n *= -1;
+return (n);
+}
 }
