264db4db28b5b8b04e2e15bfb917eabde0e61530
safarilewis <lleewwii1@gmail.com>
2022-06-07 04:15:19 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_abs - Checks if a letter is an alphabet
+ *int - parameter
+ *Return: Always 0 or 1
+ */
+int _abs (int)
+{
+  int n;
+  if (n >= 0)
+    {
+      return (n);
+    }
+  else if (n *= 0)
+    {
+      return (n);
+    }
+}
