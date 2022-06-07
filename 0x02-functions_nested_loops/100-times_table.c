3e534fa8bc0bd4701de8f0bf2f3318ae4113a929
Safari <lleewwii1@gmail.com>
2022-06-07 23:13:54 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,39 +14,32 @@ for (a = 0; a <=  n; a++)
 {
 for (b = 0; b <= n; b++)
 {
-c = a * b; d = c / 100;
+c = a * b;
+d = c / 100;
 e = (c /  10) % 10;
 f = (c % 100) % 10;
 if (b == 0)
 {
-_putchar('0');
-}
+_putchar('0'); }
 else if (c < 10)
 {
 _putchar(' ');
 _putchar(' ');
-_putchar('0' + f);
-}
+_putchar('0' + f); }
 else if (c < 100)
 {
 _putchar(' ');
 _putchar('0' + e);
-_putchar('0' + f);
-}
+_putchar('0' + f); }
 else
 {
 _putchar('0' + d);
 _putchar('0' + e);
-_putchar('0' + f);
-}
+_putchar('0' + f); }
 if (b < n)
 {
 _putchar(',');
-_putchar(' ');
-}
+_putchar(' '); }
 else
 _putchar('\n');
-}
-}
-}
-}
+}}}}
