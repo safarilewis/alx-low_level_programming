4798c48c0d794b7524676fb46880e4a104848ea7
Safari <lleewwii1@gmail.com>
2022-06-07 22:48:43 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  *_islower - Checks if a letter is lower case
- *
+ *@c: Variable
  *Return: 0 or 1
  */
 int _islower(int c)
 {
-if(c >= 'a' && c <= 'z')
+if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
