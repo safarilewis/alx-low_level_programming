4b11a297bb55f86da2ce4d1fc4e2daad6f6181b0
Safari <lleewwii1@gmail.com>
2022-06-07 23:04:44 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 /**
  *add - Prints numbers up to 98
+ *@n: Parameter
  */
 void print_to_98(int n)
 {
-  while (n != 98)
-    {
-      printf("%i, ", n);
-      if (n > 98)
-	n--;
-      else
-	n++;
-    }
-  printf("98\n");
+while (n != 98)
+{
+printf("%i, ", n);
+if (n > 98)
+n--;
+else
+n++;
+}
+printf("98\n");
 }
