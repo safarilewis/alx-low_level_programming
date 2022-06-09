c16a3b3c53cf4ecb7e5a4fde63b391c9d931082a
safarilewis <lleewwii1@gmail.com>
2022-06-09 09:23:43 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isupper - checks whether something is uppercase
+ *@c: variable
+ *Return: Always zero
+ */
+int _isUpper(int c)
+{
+if (c >=m 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}}
