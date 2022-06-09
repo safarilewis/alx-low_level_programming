2765b4b8501e7ad738613cf8aeff16499a0715b4
Safari <lleewwii1@gmail.com>
2022-06-10 00:47:37 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 int i;
-printf ("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 i = 0;
 /**
 *while (i < 10)
@@ -14,6 +14,6 @@ i = 0;
 *putchar(i);
 *}
 */
-printf ("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
