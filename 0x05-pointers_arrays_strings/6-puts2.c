ea2ab038b83af0fb3237ca47ead90801d7fc6611
Safari <lleewwii1@gmail.com>
2022-06-13 22:05:02 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a  string to stdout
- * @str: string to print
- *
- * Return: void
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
  */
 void puts2(char *str)
 {
-int i = 0;
-while (*(str + i))
+int len, i;
+len = 0;
+while (str[len] != '\0')
 {
-_putchar(*(str + i));
-i = i + 2;
+len++;
+}
+for (i = 0; i < len; i += 2)
+{
+_putchar(str[i]);
 }
 _putchar('\n');
 }
