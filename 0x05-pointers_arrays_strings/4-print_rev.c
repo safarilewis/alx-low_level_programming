489961fd9a8720b73b03776628bb79de519505b3
safarilewis <lleewwii1@gmail.com>
2022-06-13 04:38:53 -0700

    Printing in revverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_rev - Entry point
+ *@s: Parameter
+ *Return: Nothing
+ */
+void print_rev(char *s)
+{
+int x = -1;
+while (*(s+x))
+{
+_putchar(*(s+x));
+i--;
+}
+_putchar('\n');
+}
