493ab2f058078a4ca936e59675b08e9f7adde213
Safari <lleewwii1@gmail.com>
2022-06-14 12:26:14 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,13 +4,15 @@
  *
  * Return: void
  */
-void more_numbers(void)
+void more_numbers (void)
 {
 int i, j;
-  for (i = 0; i <= 14; i++)
-  {
-    for (j = 0; j < 10; j++)
-      _putchar(i);
-    }
-  _putchar('\n');
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
 }
