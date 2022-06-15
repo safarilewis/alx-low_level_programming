fb474d7fd618cd908bb98bb7648e7e067b45617a
Safari <lleewwii1@gmail.com>
2022-06-15 22:15:27 +0300

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_strncat - Adds two strings together
+*@dest: Parameter
+*@src: Parameter
+*@n: Parameter
+*Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+  int i,j;
+  i = j = 0;
+  while (*(dest + i))
+    i++;
+  for (j = 0; j < n && *(src + j) != '\0'; j++)
+    *(dest + i) = *(src + j);
+  return (dest);
+}
