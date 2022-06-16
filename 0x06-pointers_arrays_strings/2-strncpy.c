ea02ad239114e555d95771be64c415814a6540db
safarilewis <lleewwii1@gmail.com>
2022-06-16 01:33:58 -0700

    Fixing style

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 i = 0;
-while (i < n && *(src + i)) {
+while (i < n && *(src + i))
+{
 *(dest + i) = *(src + i);
 i++;
 }
-while (i < n) {
+while (i < n)
+{
 *(dest + i) = '\0';
 i++;
 }
