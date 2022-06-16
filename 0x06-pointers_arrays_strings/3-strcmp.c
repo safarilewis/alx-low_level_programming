dade718c03c6fb2375cdc3033cef04df9d17c6aa
safarilewis <lleewwii1@gmail.com>
2022-06-16 01:47:34 -0700

    stcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *_strcmp - Compares two strings
+ *@s1: First string
+ *@s2: Second string
+ *Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+  int less, equal, more;
+  less = -1;
+  equal = 0;
+  more = 1;
+
+  int i, j;
+  i = 0;
+  j = 0;
+
+  while (*(s1 + i))
+    i++;
+  while (*(s2 + j))
+    j++;
+  if (i > j)
+    return (more);
+  else if (i == j)
+    return (equal);
+  else
+    {
+      return (less);
+    }
+}
