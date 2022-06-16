2ad7b4fce6902f5d8936216f4d4b8dd6119c9883
safarilewis <lleewwii1@gmail.com>
2022-06-16 02:03:25 -0700

    Fixing error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,8 +7,8 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-  int n = 0;
-  while (s1[i] && s2[i] && (s1[i] == s2[i]))
-    i++;
-  return (s1[i] - s2[i]);
+int i = 0;
+while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
+i++;
+return (*(s1 + i) - *(s2 + i));
 }
