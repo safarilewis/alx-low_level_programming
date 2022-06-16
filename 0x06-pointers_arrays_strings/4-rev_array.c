9a3dfcca699688cb6425978572935f76dc3bae22
safarilewis <lleewwii1@gmail.com>
2022-06-16 04:31:43 -0700

    Project Commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,14 +7,14 @@
  */
 void reverse_array(int *a, int n)
 {
- int i = 0, t;
- n = n - 1;
- while (i < n)
- {
- t = *(a + i);
- *(a + i) = *(a + n);
- *(a + n) = t;
- i++;
- n--;
- }
- }
+int i = 0, t;
+n = n - 1;
+while (i < n)
+{
+t = *(a + i);
+*(a + i) = *(a + n);
+*(a + n) = t;
+i++;
+n--;
+}
+}
