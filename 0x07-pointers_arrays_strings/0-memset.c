84048e2046893244a5585b02d4ccb41502048465
Safari <lleewwii1@gmail.com>
2022-06-20 12:46:48 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,10 +8,10 @@
  */
 char *_memset(char *s, char b, usigned int n)
 {
-  int i;
-  for (i = 0; i < n; i++)
-    {
-      *(s+i) = b;
-    }
-  return (s);
+int i;
+for (i = 0; i < n; i++)
+{
+*(s+i) = b;
+}
+return (s);
 }
