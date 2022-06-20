ac4aa356632602bcce3efa0b72257342125ad30a
safarilewis <lleewwii1@gmail.com>
2022-06-20 03:56:11 -0700

    Project Commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_memcpy - Copies n bytes from src and puts them in dest
+ *@dest: Destination
+ *@src: Source
+ *@n: Bytes copied
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+*(dest + i) = *(src + i);
+}
+return (dest);
+}
