f20a0d0b93f01fb2d02b2a507ec2c7dd08b01e54
safarilewis <lleewwii1@gmail.com>
2022-06-22 04:33:23 -0700

    Project Commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,11 +6,9 @@
  */
 int _strlen_recursion(char *s)
 {
-  int i;
-  i = 0;
   if (*s != '\0')
     {
-      _strlen_recursion(s + i++);
+      return (1 + _strlen_recursion(s + 1);
     }
-  return (i);
+	return (0);
 }
