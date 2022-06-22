3f446740fb66f881c924bd564e16a433848a94cd
safarilewis <lleewwii1@gmail.com>
2022-06-22 02:37:01 -0700

    Project Commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,7 +6,8 @@
  */
 void _puts_recursion(char *s)
 {
-  if (s == '\0')
-    return;
-  return (_puts_recursion(*(s + 1)));
+  if (*s == '\0')
+    _putchar('\n');
+  _putchar(s + 1);
+  _putchar('\n');
 }
