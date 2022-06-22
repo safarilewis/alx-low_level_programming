51a849e39c5d5971da5cb9007952d108f0c23f37
safarilewis <lleewwii1@gmail.com>
2022-06-22 04:59:56 -0700

    Project Commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_pow_recursion - Returnsx to the power of y
+ *@x: base
+ *@y: exponent
+ *Return: power
+ */
+int _pow_recursion(int x, int y)
+{
+  if (y < 0)
+    {
+      return (-1);
+    }
+  return (x ** y);
+}
