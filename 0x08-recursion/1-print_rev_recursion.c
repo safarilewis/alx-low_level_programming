dc253869358a234913753f7e5f9693ab73430a0f
safarilewis <lleewwii1@gmail.com>
2022-06-22 03:53:18 -0700

    Project Commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - Printing a string in reverse using recursion
+ *@s: String to be printed in reverse
+ *Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+  int i;
+  i = 0;
+  if (*s != '\0')
+    {
+      i = i + 1;
+      _print_rev_recursion(s + 1);
+    }
+  if (i != 0)
+    {
+      _putchar(s);
+      _print_rev_recursion(s - 1);
+      i = i -1;
+    }
+}
