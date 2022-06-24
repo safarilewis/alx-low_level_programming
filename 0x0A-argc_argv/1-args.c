ee81e32e0aba7cda56a42185e8ca87c0eb218040
Safari <lleewwii1@gmail.com>
2022-06-25 00:03:04 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,13 @@
-#include <stio.h>
+#include <stdio.h>
 /**
- *main - Entry point
+ * main - prints the name number of arguments passed to it
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
  */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
