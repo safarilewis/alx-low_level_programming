1e13597900a1f68f6e80ed9e59159c598648a544
safarilewis <lleewwii1@gmail.com>
2022-06-24 14:00:17 -0700

    Project Commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  *main - Entry point
+ *@argv: Array of arguments
  *Return: NULL
  */
-int main(int argc, char *argv[])
+int main(char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
