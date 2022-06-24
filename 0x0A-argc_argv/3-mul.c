668ffa64e916f7f8b3e4b0dce9a2bfa9ed550f0f
Safari <lleewwii1@gmail.com>
2022-06-25 00:10:45 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,12 +12,12 @@ int main(int argc, char *argv[])
 int a, b, c;
 if (argc != 3)
 {
-puts("Error");
+puts ("Error");
 return (1);
 }
-a = atoi(argv[1]);
-b = atoi(argv[2]);
+a = atoi (argv[1]);
+b = atoi (argv[2]);
 c = a * b;
-printf("%d\n", c);
+printf ("%d\n", c);
 return (0);
 }
