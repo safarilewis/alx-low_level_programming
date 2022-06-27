34288ce8d9ef9c756ec8f8ee1cedd92e7caa1e30
safarilewis <lleewwii1@gmail.com>
2022-06-27 03:50:24 -0700

    ALX Project Commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup - Copy a string and return pointer to new string
+ *@str: String to be copied
+ *Return: Null or Pointer to new string
+ */
+char *_strdup(char *str)
+{
+  unsigned int i, j;
+  char *s;
+  for (i = 0; str[i]; i++)
+    ;
+  if (str == NULL)
+    return (NULL);
+  s = malloc(i * sizeof(char));
+  for (j = 0; j < i; j++)
+    {
+      s[j] = str[i];
+    }
+  return (s);
