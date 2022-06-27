283f968c8e35947494834e555fdda31a11fd4d5a
safarilewis <lleewwii1@gmail.com>
2022-06-27 04:27:37 -0700

    ALX Project Commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *alloc_grid -  Create a 2D Array and return pointer
+ *@width: Width of array
+ *@height: Height of the array
+ *Return: Dimensions of the array or NULL
+ */
