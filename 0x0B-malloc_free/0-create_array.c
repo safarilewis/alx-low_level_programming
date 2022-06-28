fc791ad91d7dd42ed0713f8fe2ce8af3fbcaea34
Safari <lleewwii1@gmail.com>
2022-06-28 20:39:12 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,18 +8,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  char *ar;
-  unsigned int i;
-  if (size == 0)
-	       return (NULL);
+char *ar;
+unsigned int i;
+if (size == 0)
+return (NULL);
 ar = malloc(size * sizeof(char));
-  if (ar == NULL)
-    {
-      return (NULL);
-    }
-  for (i = 0; i < size; i++)
-    {
-      ar[i] = c;
-    }
-  return (ar);
+if (ar == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+ar[i] = c;
+}
+return (ar);
 }
