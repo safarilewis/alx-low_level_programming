c88e62174e0b2113d144d09f4cfda22a781bbd3c
Safari <lleewwii1@gmail.com>
2022-06-28 21:05:40 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,11 +17,8 @@ for (i = 0; str[i]; i++)
 ;
 i++;
 s = malloc(i * sizeof(char));
-<<<<<<< HEAD
 if (s == NULL)
 return (NULL);
-=======
->>>>>>> 68cdb5b149e637395d3651f01339be921b98089c
 for (j = 0; j < i; j++)
 {
 s[j] = str[j];
