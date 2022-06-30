cb3e95b6b33e8edacb1f9cfcbc5a458ac3958d1a
Safari <lleewwii1@gmail.com>
2022-06-30 14:24:45 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,19 +11,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int i, j, k;
 	char *s3;
-	s3 = malloc(sizeof(s1) + n *sizeof(char) + 1);
-	for (i = 0; *(s1 + i); i++)
+	if (s1 == NULL)
 	{
-		s3[i] = s1[i];
+		i = 0;
 	}
-	for (j = 0; j <= n; j++)
+	else
 	{
-		*(s3 + i + j) = s2[j];
+		for (i = 0; s1[i]; i++)
+			;
 	}
-	for (k = 0; k<= j; k++)
-	  ;
-	s3[i + j + 1] = '\0';
+	if (s2 == NULL)
+	{
+		j = 0;
+	}
+	else
+	{
+		for (j = 0; j < n; j++)
+			;
+	}
+	s3 = malloc(sizeof(char) * (i + j + 1));
 	if (s3 == NULL)
-		return (NULL);
+		return (NULL)
+	for (k = 0; k < i; k++)
+		s3[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s3[k + i] = s2[k];
+	s3[i + j] = '\0';
 	return (s3);
 }
