60d661bc53b1158a4fdd57d7cf80437e64a3361c
Lewis Safari <lleewwii1@gmail.com>
2022-08-18 09:44:38 +0300

    Fixing code for more functions and loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,11 @@
 */
 void print_most_numbers(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-if (c != '2' && c != '4')
-{
-_putchar(c);
-}
-}
-_putchar('\n');
+  char c;
+  while (c <= '9') & ((c != '2') || (c != '4'))
+		       {
+			 _putchar(c);
+			 c++;
+		       }
+  _putchar('\n');
 }
