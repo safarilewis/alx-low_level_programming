3eee98c20992231643ecc881a2de37ef7d562a74
Lewis Safari <lleewwii1@gmail.com>
2022-08-18 10:51:29 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,15 +4,25 @@
  *
  * Return: void
  */
+
+
 void more_numbers(void)
 {
-char i, j;
-for (j = '0'; j < '10'; j++)
-{
-for (i = '0'; i <= '14'; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
-}
+	char a, b, c, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c < 10)
+				b = c;
+			else
+			{
+				a = c / 10; b = c % 10;
+				_putchar('0' + a);
+			}
+			_putchar('0' + b);
+		}
+		_putchar('\n');
+	}
 }
