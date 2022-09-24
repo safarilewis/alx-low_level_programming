112a8a4a267c1d851e30ea6105e33dfd597d4bc1
Lewis Safari <lleewwii1@gmail.com>
2022-09-23 22:14:07 -0400

    Fixing header file error

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,4 +42,4 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-#endeif /*_HASH_TABLES_H_*/
+#endif /*_HASH_TABLES_H_*/
