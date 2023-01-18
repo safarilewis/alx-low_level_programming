345667a26b10518a0c012c590544a6e3cab4ced9
safarilewis <lleewwii1@gmail.com>
2023-01-18 10:29:39 +0300

    search algos

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+/**
+ * linear_search - Implements a linear search algorithm
+ * @array: Pointer to first element of array
+ * @size: Number of elements in array
+ * @value: Value being searched for in the array
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+    unsigned int i;
+    if (!array)
+        return (-1);
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array[%d] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return i;
+    }
+    return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Implements a binary search algorithm
+ * @array: Pointer to the first element of array to be searched
+ * @size: Size of the array being searched
+ * @value: Value being looked for in the array
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+    
+}
